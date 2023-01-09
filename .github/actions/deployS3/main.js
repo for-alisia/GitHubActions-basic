fd0bcda92199497089c385ad6e877b3c21d4b8b8
Elena <for-alisia@yandex.ru>
2023-01-09 15:35:21 +0100

    Added aws action

--- .github/actions/deployS3/main.js
+++ .github/actions/deployS3/main.js
@@ -1,8 +1,17 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+  // Get inputs
+  const bucket = core.getInput('bucket', { required: true });
+  const region = core.getInput('bucket-region', { required: true });
+  const dist = core.getInput('dist-folder', { required: true });
+
+  // Upload dist folder
+  const s3Uri = `s3://${bucket}`
+
+  exec.exec(`aws s3 sync ${dist} ${s3Uri} --region ${region}`)
+
   core.notice('Output from JS custom action')
 }
 
